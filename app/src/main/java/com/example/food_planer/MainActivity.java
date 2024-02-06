fb53e11fd51c4cd8511ee606d3874d457e0e5ce4
naden <nadenms783@gmail.com>
2024-02-06 08:40:04 +0200

    start project

--- app/src/main/java/com/example/food_planer/MainActivity.java
+++ app/src/main/java/com/example/food_planer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.food_planer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: ");
         
     }
 }
