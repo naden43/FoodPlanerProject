8d469f954c0ae8498cb392d3938171630b4a169e
naden <nadenms783@gmail.com>
2024-02-07 18:49:34 +0200

    UI of Login and Register

---
+++ app/src/main/java/com/example/food_planer/login/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.food_planer.login.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.food_planer.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
